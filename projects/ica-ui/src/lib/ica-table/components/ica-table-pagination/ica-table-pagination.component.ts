bd22756eb93620edea3912b8952f0c765b311df9
Mark <markberry867@gmail.com>
2018-11-15 23:15:47 -0600

    Fix paginator page index

--- projects/ica-ui/src/lib/ica-table/components/ica-table-pagination/ica-table-pagination.component.ts
+++ projects/ica-ui/src/lib/ica-table/components/ica-table-pagination/ica-table-pagination.component.ts
@@ -174,9 +174,18 @@ export class IcaTablePaginationComponent implements OnInit {
    *
    */
   public getRangeLabel(pageIndex: number, pageSize: number, length: number) {
-    const start = (pageIndex * pageSize) + 1
-    const end = Math.min((pageIndex * pageSize) + 1 + pageSize, length)
-    return `${start} - ${end} of ${length}`
+    if (length === 0 || pageSize === 0) { return `0 of ${length}` }
+
+    length = Math.max(length, 0)
+
+    const startIndex = pageIndex * pageSize
+
+    // If the start index exceeds the list length, do not try and fix the end index to the end.
+    const endIndex = startIndex < length ?
+        Math.min(startIndex + pageSize, length) :
+        startIndex + pageSize
+
+    return `${startIndex + 1} - ${endIndex} of ${length}`
   }
 
   /**
